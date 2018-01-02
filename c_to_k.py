98bdc548325f80f75d33d84f2930c314745a1d5a
THeMysterion <dorianpolska153@gmail.com>
2018-01-02 17:33:46 +0100

    Add files via upload

---
+++ c_to_k.py
@@ -0,0 +1,18 @@
+#celcuis_to_kelvin
+
+class TooColdException(Exception):
+
+	def __init__(self):
+		print("Temp:")
+
+
+def c_to_k(temp):
+	
+	if temp < -273.15:
+		raise TooColdException("Temp is -0")
+	return temp + 273.15
+		
+print(c_to_k(-300))
+	
+
+	
